083665f71bcdba2621ba3ad5f45cca1d97af6712
gdrodrigues <39449297+gdrodrigues@users.noreply.github.com>
2018-06-15 16:32:24 -0300

    Add files via upload

---
+++ gerente_Supreme.sql
@@ -0,0 +1,25 @@
+﻿CREATE OR REPLACE FUNCTION gerenteSupreme()returns TRIGGER AS $$
+
+DECLARE
+c1_funcionarios CURSOR for select*from funcionario WHERE funcionario.cargo<>'gerente' and new.agencia=funcionario.agencia;
+r1 funcionario%rowtype;
+c2_funcionarios CURSOR for select*from funcionario WHERE funcionario.cargo='gerente'and new.agencia=funcionario.agencia;
+r2 funcionarios%rowtype;
+
+BEGIN
+
+	for r1 in c1_funcionarios LOOP		
+		if new.salario<r1.salario or new.vale_alimentacao<r1.vale_alimentacao or new.p_saude<r1.p_saude or new.n_dependentes<r1._dependentes THEN
+			RAISE EXCEPTION 'Os benefícios do gerente não estão de acordo com a política salarial da empresa'
+
+		END IF;
+	END LOOP;
+
+	return new;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+-- CREATE TRIGGER gerenteSupreme() BEFORE INSERT OR UPDATE ON FUNCIONARIO
+-- 	FOR EACH ROW EXECUTE PROCEDURE gerenteSupreme();
