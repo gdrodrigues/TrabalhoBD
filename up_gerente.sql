b9cb1fc4538fdef6d0fa7c16990e90c562569511
gdrodrigues <39449297+gdrodrigues@users.noreply.github.com>
2018-06-17 12:43:21 -0300

    Update up_gerente.sql

--- up_gerente.sql
+++ up_gerente.sql
@@ -1,11 +1,26 @@
-﻿CREATE OR REPLACE FUNCTION up_gerente() returns trigger AS &&
+CREATE OR REPLACE FUNCTION up_gerente() returns trigger AS &&
 
 DECLARE
 
 BEGIN
 
-	IF new.cago='gerente' THEN
+	IF (TG_OP= 'INSERT' and new.cago='gerente') THEN
 		INSERT into GERENTE(cpf) values (new.cpf);
+		
+
+	ELSIF TG_OP= 'UPDATE'
+	
+		if funcionario.cargo='comum' and new.cargo='gerente' THEN
+			INSERT into GERENTE(cpf) values (new.cpf);
+		END IF;
+
+		if funcionario.cargo='gerente' and new.cargo='comum' THEN
+			DELETE FROM gerente WHERE gerente.cpf=new.cpf;
+		END IF;
+		
+	END IF;
+	
+	return new;
 
 END
 $$
