24d88bdcbefeee22ac3f0e6ac4b9c2f6de04567d
gdrodrigues <39449297+gdrodrigues@users.noreply.github.com>
2018-06-16 23:31:01 -0300

    Add files via upload

---
+++ limite_dep.sql
@@ -0,0 +1,37 @@
+﻿Create or replace function limite_dependentes() returns trigger AS $$
+
+DECLARE
+
+c_funcionario cursor for select*from funcionario where funcionario.cpf=new.cpf;
+contador integer;
+
+BEGIN
+	IF c_funcionario.cargo='comum' THEN
+	
+		select count(*) from dependente where dependente.cpf=new.cpf into contador;
+		
+		IF contador=2 THEN
+		
+			RAISE EXCEPTION 'O funcionário já possui o número máximo de dependentes registrado';
+		ELSE
+			return new;
+			
+		END IF;
+
+	ELSE	
+		select count(*) from dependente where dependente.cpf=new.cpf into contador;
+		
+		IF contador=4 THEN
+		
+			RAISE EXCEPTION 'O gerente já possui o número máximo de dependentes registrado';
+		ELSE
+			return new;
+			
+		END IF;
+	END IF;	
+END;
+$$
+LANGUAGE plpgsql;
+
+--CREATE TRIGGER limite_dependentes BEFORE INSERT on dependente
+ --	FOR EACH ROW EXECUTE PROCEDURE limite_dependetes();
---
+++ n_dependentes.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION n_dependentes() returns TRIGGER AS $$
+
+-- trigger que garante o valor correto do atributo derivado "n_dependentes" da entidade funcionario
+
+DECLARE
+
+BEGIN
+	UPDATE funcionario SET funcionario.n_dependentes = funcionario.n_dependentes+1 WHERE funcionario.cpf=new.cpf; 
+
+END;
+$$
+LANGUAGE plpgsql;
+
+-- CREATE TRIGGER n_dependentes AFTER INSERTION on DEPENDENTE
+--	FOR EACH ROW EXECUTE PROCEDURE n_dependentes();
