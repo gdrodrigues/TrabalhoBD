27cfc4202294e18b3525ca7abefa9e3e1c5f566c
gdrodrigues <39449297+gdrodrigues@users.noreply.github.com>
2018-06-17 12:30:34 -0300

    Add files via upload

---
+++ up_gerente.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION up_gerente() returns trigger AS &&
+
+DECLARE
+
+BEGIN
+
+	IF new.cago='gerente' THEN
+		INSERT into GERENTE(cpf) values (new.cpf);
+
+END
+$$
+LANGUAGE plpgsql;
+
+-- CREATE TRIGGER up_gerente AFTER INSERT on FUNCIONARIO
+--	FOR EACH ROW EXECUTE PROCEDURE up_gerente();
